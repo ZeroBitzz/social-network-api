37d9df0df4d4e6741b4d6a2f41ebdc47d30423fe
Broderick <35615510+ZeroBitzz@users.noreply.github.com>
2023-04-30 19:26:10 -0600

    thoughts model

---
+++ models/Thought.js
@@ -0,0 +1,71 @@
+import mongoose from 'mongoose';
+const Schema = mongoose.Schema;
+
+const ReactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new mongoose.Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (timestamp) => dateFormat(timestamp),
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
+);
+
+const ThoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minlength: 1,
+      maxlength: 280
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: timestamp => dateFormat(timestamp)
+    },
+    username: {
+      type: String,
+      required: true
+    },
+    reactions: [ReactionSchema]
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true
+    },
+    id: false
+  }
+);
+
+
+ThoughtSchema.virtual('reactionCount').get(function() {
+  return this.reactions.length;
+});
+
+const dateFormat = (timestamp) => {
+  return new Date(timestamp).toLocaleString();
+};
+
+const Thought = mongoose.model('Thought', ThoughtSchema);
+
+export default Thought;
